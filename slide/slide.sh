037de06ab06dc8d7bcbf9fd02085cc5cf894840c
leoddias <leoddias@live.com>
2021-08-20 18:22:37 -0300

    Update slide project

--- slide/slide.sh
+++ slide/slide.sh
@@ -9,9 +9,11 @@ do
                             <img src=\"$IMG_BASE64\">
                         </div>
     "
-    echo "$DIV_HTML" >> images.html
+    echo "$DIV_HTML" >> tmp.html
 done
 
+sed -i '/<!-- CONTENT HERE-->/e cat tmp.html' slide.html
+rm tmp.html
 echo "DONE"
-echo "Please copy everything inside images.html and paste in slide.html, then use staticrypt to generate your protected slide webpage"
+echo "If you want to encrypt your page with a secret you can use staticrypt"
 echo "Staticrypt can be found at: https://robinmoisson.github.io/staticrypt/"
