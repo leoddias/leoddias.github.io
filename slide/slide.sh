756359857531a89d79735b7803758560e8898519
leoddias <leoddias@live.com>
2021-08-21 00:11:03 -0300

    Update slide project

--- slide/slide.sh
+++ slide/slide.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 IMAGES='images/*'
+
 for image in $IMAGES
 do
     echo "Processing image $image"
-    IMG_BASE64="data:image/jpeg;base64,$(base64 -w 0 $image)"
+    IMG_BASE64=$(base64 -w 0 "$image")
     DIV_HTML="
                         <div class=\"card-image\">
-                            <img src=\"$IMG_BASE64\">
+                            <img src=\"data:image/jpeg;base64,$IMG_BASE64\">
                         </div>
     "
     echo "$DIV_HTML" >> tmp.html
 done
 
-sed -i '/<!-- CONTENT HERE-->/e cat tmp.html' slide.html
+sed '/<!-- CONTENT HERE-->/e cat tmp.html' template.html > slide.html
 rm tmp.html
-echo "DONE"
+echo "DONE, check slide.html file in your browser"
 echo "If you want to encrypt your page with a secret you can use staticrypt"
 echo "Staticrypt can be found at: https://robinmoisson.github.io/staticrypt/"
